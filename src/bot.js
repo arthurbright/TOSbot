545258caeae05f1fdc35b2a2d022dfe07c081569
arthurbright <55501753+arthurbright@users.noreply.github.com>
2021-05-22 17:59:12 -0400

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+//making new discord bot client
+const { Client } = require('discord.js');
+const client = new Client();
+
+//bot login, bot goes online
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
+//on ready method runs when bot first logs on
+client.on('ready', () => {
+    console.log("Bot successfully logged in: " + client.user.tag);
+});
+
+//when a message is sent
+client.on('message', (message) =>{
+    if(message.author.discriminator === '8517'){
+        message.channel.send(message.content);
+        message.delete();
+    }
+
+    console.log(message.channel.name);
+});
+
+
